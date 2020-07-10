eee74386afe5865d76d7ab0a43a662d3a8221f20
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-07-10 17:19:50 +0600

    background job span added

--- src/Middleware/RecordBackgroundTransaction.php
+++ src/Middleware/RecordBackgroundTransaction.php
@@ -2,11 +2,36 @@
 
 namespace Anik\ElasticApm\Middleware;
 
+use Anik\ElasticApm\Agent;
+use Anik\ElasticApm\Spans\BackgroundJobSpan;
+use Anik\ElasticApm\Transaction;
+use Carbon\Carbon;
 use Closure;
 
 class RecordBackgroundTransaction
 {
-    public function handle ($request, Closure $next) {
-        return $next($request);
+    public function handle ($job, Closure $next) {
+        // set transaction for the job
+        $transaction = new Transaction();
+        $transaction->setName($this->getTransactionName($job))->setType($this->getTransactionType());
+        app('apm-agent')->setTransaction($transaction);
+
+        // Set span for the job it's processing
+        $jobClass = get_class($job);
+        app('apm-agent')->addSpan(new BackgroundJobSpan($jobClass, 'processing', Carbon::now()->toDateTimeString()));
+        $response = $next($job);
+        // set span for the class which is processed
+        app('apm-agent')->addSpan(new BackgroundJobSpan($jobClass, 'processed', Carbon::now()->toDateTimeString()));
+        app('apm-agent')->capture();
+
+        return $response;
+    }
+
+    private function getTransactionName ($job) {
+        return get_class($job);
+    }
+
+    private function getTransactionType () {
+        return config('elastic-apm.transaction.type.background', 'queue');
     }
 }
---
+++ src/Spans/BackgroundJobSpan.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Anik\ElasticApm\Spans;
+
+use Anik\ElasticApm\Contracts\SpanContract;
+
+class BackgroundJobSpan implements SpanContract
+{
+    use SpanEmptyFieldsTrait;
+    private $job, $time, $state;
+
+    public function __construct (string $job, string $state, string $time) {
+        $this->job = $job;
+        $this->state = $state;
+        $this->time = $time;
+    }
+
+    public function getName () : string {
+        return $this->state;
+    }
+
+    public function getType () : string {
+        return config('elastic-apm.types.background', 'job');
+    }
+
+    public function getSubType () : string {
+        return $this->job;
+    }
+
+    public function getLabels () : array {
+        return $this->getSpanData();
+    }
+
+    public function getSpanData () : array {
+        return [
+            'time'  => $this->time,
+            'state' => $this->state,
+        ];
+    }
+}
